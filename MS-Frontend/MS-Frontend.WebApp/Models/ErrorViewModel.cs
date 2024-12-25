b6b279fe991be8bd7440e0b7f8f8a0a1f25d2547
Kyle Morton <mkmorton91@gmail.com>
2024-12-25 13:15:27 -0600

    init

---
+++ MS-Frontend/MS-Frontend.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MS_Frontend.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
